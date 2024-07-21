577c30b66ce6bdd36193573e239132933ea6f21e
shanakaprageeth <shanakaprageeth@gmail.com>
2024-07-21 13:53:35 +0900

    [fix]: character device

--- drivers/dummy_drv/dummy_drv.c
+++ drivers/dummy_drv/dummy_drv.c
@@ -2,83 +2,102 @@
 #include <linux/init.h>
 #include <linux/string.h>
 #include <linux/printk.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
 
 #include "dummy_drv.h"
 
-#define DRV_NAME "DUMMY_DRV"
-#define DRV_MODULE_VERSION "1.0"
-#define DRV_MODULE_AUTHOR "Shanaka"
-#define DRV_MODULE_DESCRIPTION "Test driver with kernel messages"
-
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE(LICENSE_V);
 MODULE_AUTHOR(DRV_MODULE_AUTHOR);
 MODULE_DESCRIPTION(DRV_MODULE_DESCRIPTION);
 MODULE_VERSION(DRV_MODULE_VERSION);
 
+static ssize_t return_data_to_user(struct file *File, char *user_buffer, size_t count, loff_t *offset){
+	int to_copy, not_copied, delta;
+	to_copy = min(count, buffer_pointer);
+	not_copied = copy_to_user(user_buffer, buffer, to_copy);
+	delta = to_copy - not_copied;
+	printk(KERN_DEBUG "Copy data to user buffer \n");
+	return delta;
+}
 
-/**
- * @brief This function notify when the device driver file is open from userland
- * @param struct inode pointer to device_file instance
- * @param struct file pointer to a file instance
- * @return int 0-success
- */
+static ssize_t process_user_data(struct file *File, const char *user_buffer, size_t count, loff_t *offset){
+	int to_copy, not_copied, delta;
+	to_copy = min(count, sizeof(buffer));
+	not_copied = copy_from_user(buffer, user_buffer, to_copy);
+	buffer_pointer = to_copy;
+	delta = to_copy - not_copied;
+	printk(KERN_DEBUG "Processed user data \n");
+	return delta;
+}
+
 static int user_device_open(struct inode *device_file, struct file *instance){
-	printk(KERN_NOTICE "Device /dev/file was opened\n");
+	printk(KERN_NOTICE "Device /dev/ was opened\n");
 	return 0;
 }
 
-/**
- * @brief This function notify when the device driver file is closed from userland
- * @param struct inode pointer to device_file instance
- * @param struct file pointer to a file instance
- * @return int 0-success
- */
 static int user_device_release(struct inode *device_file, struct file *instance){
-	printk(KERN_NOTICE "Device /dev/file was closed\n");
+	printk(KERN_NOTICE "Device /dev/ was closed\n");
 	return 0;
 }
 
-/**
- * @brief structure to save the file operations functions
- * @param owner owner of the operation
- * @param open open operation function
- * @param release release operation function
-*/
-static struct file_operations file_ops = {
-	.owner = THIS_MODULE,
-	.open = user_device_open,
-	.release = user_device_release
-};
-
-/**
- * @brief this function initialize the module
- * @return int 0-success 1-failure
-*/
-static int __init dummy_init(void){
+static int __init ModuleInit(void){
+	int retval = 0;
 	printk("%s()\n", __func__);
 	printk(KERN_NOTICE "Loading " DRV_NAME " Driver \n");
+	// TODO make device node number dynamic
 	// register the device to userland
-    int retval = register_chrdev(99, "device_number",&file_ops );
+	if (alloc_chrdev_region(&device_no, 0, 1, DEVICE_NAME) <0){
+		printk(KERN_ALERT "failed to register the char device %s\n",DEVICE_NAME);
+		return -1;
+	}
+	printk(KERN_NOTICE "successufully registered device %d\n", device_no);
+	if ((device_class = class_create(DEVICE_CLASS)) == NULL){
+		printk(KERN_ALERT "failed to create device class %s\n",DEVICE_CLASS);
+		unregister_chrdev_region(device_no, 1);
+		return -1;
+	}
+	if ((device_create(device_class, NULL, device_no, NULL, DEVICE_NAME)) == NULL){
+		printk(KERN_ALERT "failed to create device file  %s %d", DEVICE_NAME, device_no);
+		class_destroy(device_class);
+		unregister_chrdev_region(device_no, 1);
+		return -1;
+	}
+	cdev_init(&device_cdev, &file_ops);
+	if (cdev_add(&device_cdev, device_no, 1) == -1){
+		printk(KERN_ALERT "failed to register device %s %d", DEVICE_NAME, device_no);
+		class_destroy(device_class);
+		unregister_chrdev_region(device_no, 1);
+		device_destroy(device_class, device_no);
+		return -1;
+	}
+    /* 
+	// Following commented section is to setup a dummy device with static device number
+	int retval = register_chrdev(99, "device_number",&file_ops );
 	if(retval < 0)
-			printk(KERN_NOTICE "failed to register the device \n");
+		printk(KERN_NOTICE "failed to register the device \n");
 	else
-			printk(KERN_NOTICE "successufully registered device \n");
+		printk(KERN_NOTICE "successufully registered device %d %d\n", retval>>20, retval&0xfffff);
+	*/
 	return retval;
+
 }
 
-/**
- * @brief this function remove the module
- * @return int 0-success 1-failure
-*/
-static void __exit dummy_exit(void){
+static void __exit ModuleExit(void){
 	printk("%s()\n", __func__);
-	unregister_chrdev(99, "device_number");
-			printk(KERN_NOTICE "Unloading " DRV_NAME " Driver \n");
+	cdev_del(&device_cdev);
+	class_destroy(device_class);
+	unregister_chrdev_region(device_no, 1);
+	device_destroy(device_class, device_no);
+	// Following commented section is to setup a dummy device with static device number
+	//unregister_chrdev(99, "device_number");
+	printk(KERN_NOTICE "Unloading " DRV_NAME " Driver \n");
 }
 
 /**
  * Configure module initialization and exit functions
  */
-module_init(dummy_init);
-module_exit(dummy_exit);
+module_init(ModuleInit);
+module_exit(ModuleExit);
   
--- drivers/dummy_drv/dummy_drv.h
+++ drivers/dummy_drv/dummy_drv.h
@@ -1,5 +1,84 @@
-#if  !defined(DUMMY_DRV_H)
+#if !defined(DUMMY_DRV_H)
 #define DUMMY_DRV_H
+
+#define LICENSE_V "GPL"
+#define DRV_NAME "DUMMY_DRV"
+#define DRV_MODULE_VERSION "1.0"
+#define DRV_MODULE_AUTHOR "Shanaka"
+#define DRV_MODULE_DESCRIPTION "Test driver with kernel messages"
+
+#define DEVICE_NAME "dummy_device"
+#define DEVICE_CLASS "char_device"
+
+/* Userland device properties*/
+static dev_t device_no;
+static struct class *device_class;
+static struct cdev device_cdev;
+
+/* Buffer for device char*/
+static char buffer[255];
+static size_t buffer_pointer = 0;
+
+/**
+ * @brief Return kernel data back to user buffer
+ * @param input file pointer
+ * @param char buffer pointer to access user data
+ * @param size size to read
+ * @param longlong offset
+ */
+static ssize_t return_data_to_user(struct file *File, char *user_buffer, size_t count, loff_t *offset);
+
+/**
+ * @brief Process user buffer data in kernel
+ * @param input file pointer
+ * @param char buffer pointer to access user data
+ * @param size size to read
+ * @param longlong offset
+ */
+static ssize_t process_user_data(struct file *File, const char *user_buffer, size_t count, loff_t *offset);
+
+/**
+ * @brief This function notify when the device driver file is open from userland
+ * @param struct inode pointer to device_file instance
+ * @param struct file pointer to a file instance
+ * @return int 0-success
+ */
+static int user_device_open(struct inode *device_file, struct file *instance);
+
+/**
+ * @brief This function notify when the device driver file is closed from userland
+ * @param struct inode pointer to device_file instance
+ * @param struct file pointer to a file instance
+ * @return int 0-success
+ */
+static int user_device_release(struct inode *device_file, struct file *instance);
+
+/**
+ * @brief structure to save the file operations functions
+ * @param owner owner of the operation
+ * @param open open operation function
+ * @param release release operation function
+*/
+static struct file_operations file_ops = {
+	.owner = THIS_MODULE,
+	.open = user_device_open,
+	.release = user_device_release,
+    .read = return_data_to_user,
+    .write = process_user_data
+};
+
+/**
+ * @brief this function initialize the module
+ * @return int 0-success 1-failure
+*/
+static int __init ModuleInit(void);
+
+
+/**
+ * @brief this function remove the module
+ * @return int 0-success 1-failure
+*/
+static void __exit ModuleExit(void);
 
 
 #endif
